f6bb20e617a8ed3bc17f61933884d49beed5aa2f
Abdullah Yavas <abdullahyavas2830@gmail.com>
2020-04-06 23:33:25 +0200

    Add project files.

---
+++ CSharpCorePFCursus/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace CSharpCorePFCursus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            test();
+        }
+
+        private static void test()
+        {
+            Console.WriteLine("Hallo");
+        }
+    }
+}
