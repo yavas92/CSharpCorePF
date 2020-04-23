86fdc2ff9d3bb11174d25d56fca5b9695243f101
Abdullah Yavas <abdullahyavas2830@gmail.com>
2020-04-23 17:03:46 +0200

    Cursus Interfaces verder afgewerkt

--- CSharpCorePFCursus/Program.cs
+++ CSharpCorePFCursus/Program.cs
@@ -8,23 +8,19 @@ namespace CSharpCorePFCursus
         {
             static void Main(string[] args)
             {
-                IKost[] kosten = new IKost[4];
+                object [] kosten = new object[5];
 
                 kosten[0] = new Arbeider("Asterix", DateTime.Now, Geslacht.Man, 24.79m, 3);
                 kosten[1] = new Bediende("Obelix", DateTime.Now, Geslacht.Man, 2400.79m);
                 kosten[2] = new Manager("Idefix", DateTime.Now, Geslacht.Man, 2400.79m, 7000m);
                 kosten[3] = new Fotokopiemachine("123", 500, 0.025m);
+                kosten[4] = new string("Abdullah"); 
 
-                decimal totaleKost = 0m;
-                foreach (IKost kost in kosten)
+
+                foreach (object obj in kosten)
                 {
-                    Console.WriteLine(kost.Menselijk);
-                    Console.WriteLine(kost.Bedrag);
-                    totaleKost += kost.Bedrag;
+                    Console.WriteLine(obj is IKost);
                 }
-                Console.WriteLine(totaleKost);
-
-                //p219
             }
         }
     }
